5747613768de267a8ce7fc5bbb63bf952c112ca3
jcoll059 <jordancolledge059@gmail.com>
2022-01-25 15:43:32 -0800

    Warmup 2 done (real)

--- src/main/java/ubc/cosc322/COSC322Test.java
+++ src/main/java/ubc/cosc322/COSC322Test.java
@@ -6,6 +6,7 @@ import sfs2x.client.entities.Room;
 import ygraph.ai.smartfox.games.BaseGameGUI;
 import ygraph.ai.smartfox.games.GameClient;
 import ygraph.ai.smartfox.games.GamePlayer;
+import ygraph.ai.smartfox.games.amazons.HumanPlayer;
 
 /**
  * An example illustrating how to implement a GamePlayer
@@ -62,8 +63,10 @@ public class COSC322Test extends GamePlayer{
     public void onLogin() {
         this.userName = gameClient.getUserName(); 
         if(this.gamegui != null) { 
-        this.gamegui.setRoomInformation(this.gameClient.getRoomList()); 
-        } 
+        	this.gamegui.setRoomInformation(this.gameClient.getRoomList()); 
+        }
+        
+        this.getGameClient().joinRoom(this.gameClient.getRoomList().get(1).getName());
     }
 
     @SuppressWarnings("unchecked")
@@ -77,16 +80,21 @@ public class COSC322Test extends GamePlayer{
     	
     	System.out.println("got here");
     	
+    	System.out.println(messageType);
+    	
     	if(messageType.equals("cosc322.game-state.board"))
     	{
-    		ArrayList<Integer> board = (ArrayList<Integer>) msgDetails.get("AmazonsGameMessage.GAME_STATE");
+    		System.out.println("here?");
+    		ArrayList<Integer> board = (ArrayList<Integer>) msgDetails.get("game-state");
+    		System.out.println("what about here?");
     		this.gamegui.setGameState(board);
+    		System.out.println("or here?");
     	}
     	else if(messageType.equals(""))		//whatever it is for GameMessage.GAME_ACTION_MOVE 
     	{
-    		ArrayList<Integer> cur = (ArrayList<Integer>) msgDetails.get("AmazonsGameMessage.QUEEN_POS_CURR");
-    		ArrayList<Integer> mve = (ArrayList<Integer>) msgDetails.get("AmazonsGameMessage.QUEEN_POS_NEXT");
-    		ArrayList<Integer> arr = (ArrayList<Integer>) msgDetails.get("AmazonsGameMessage.ARROW_POS");
+    		ArrayList<Integer> cur = (ArrayList<Integer>) msgDetails.get("queen-pos-cur");
+    		ArrayList<Integer> mve = (ArrayList<Integer>) msgDetails.get("queen-pos-next");
+    		ArrayList<Integer> arr = (ArrayList<Integer>) msgDetails.get("arrow-pos");
     		this.gamegui.updateGameState(cur,mve,arr);
     	}
     	
