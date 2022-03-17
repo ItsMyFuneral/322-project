bcae880de7f74f51132919b413f17f95fb0ef33f
jcoll059 <jordancolledge059@gmail.com>
2022-03-17 00:04:34 -0700

    Fixed comment on heuristic

--- src/main/java/ubc/cosc322/search/KeepYourselfAliveHeuristic.java
+++ src/main/java/ubc/cosc322/search/KeepYourselfAliveHeuristic.java
@@ -2,16 +2,17 @@ package ubc.cosc322.search;
 
 import java.util.ArrayList;
 
-import ubc.cosc322.board.*;
 import ubc.cosc322.board.tiletypes.*;
 
-// The idea for this heuristic came from a student in the previous year's class, Jordan Ribbink.
-// He basically just suggested the idea of a heuristic based on the number of queens that are alive,
-// compared to the opponent's. (It also hails from my original idea of a heuristic based on the number
-// of moves available to each queen, which would also be fair, but feels unnecessary.)
+// This heuristic is essentially a simplified idea I had earlier this semester.
+// The original idea consisted of a heuristic whose value would be based on the number of moves
+// available to each friendly queen, minus the same number for enemy queens.
+// I decided that was too complicated (and might also over-favor the center of the board),
+// and chose to implement an algorithm based on the number of queens each player has "alive,"
+// i.e. with at least one legal move.
 // I implemented this from there.
 
-// Yes, I made the name of the class a Queen heuristic.
+// Yes, I made the name of the class a Queen reference. Oddly fitting, too.
 
 public class KeepYourselfAliveHeuristic {
     GameState state;
